249804bec42dc85de8495b9d7372829c3d204bcd
bjornoleh <bjorn.haugsgjerd@gmail.com>
2023-04-12 18:58:53 +0200

    Revert "Merge remote-tracking branch 'jonfawcett/Main' into e"
    This reverts commit bac6b920381ece9f4a26adb972d60383bcf9ff5d.

--- LoopFollow/repository/UserDefaults.swift
+++ LoopFollow/repository/UserDefaults.swift
@@ -29,10 +29,9 @@ class UserDefaultsRepository {
         "SAGE",
         "CAGE",
         "Rec. Bolus",
-        "Pred.",
-        "Carbs today"])
-    static let infoSort = UserDefaultsValue<[Int]>(key: "infoSort", default: [0,1,2,3,4,5,6,7,8,9,10])
-    static let infoVisible = UserDefaultsValue<[Bool]>(key: "infoVisible", default: [true,true,true,true,true,true,true,true,true,true,true])
+        "Pred."])
+    static let infoSort = UserDefaultsValue<[Int]>(key: "infoSort", default: [0,1,2,3,4,5,6,7,8,9])
+    static let infoVisible = UserDefaultsValue<[Bool]>(key: "infoVisible", default: [true,true,true,true,true,true,true,true,true,true])
     static let hideInfoTable = UserDefaultsValue<Bool>(key: "hideInfoTable", default: false)
     
     // Nightscout Settings
